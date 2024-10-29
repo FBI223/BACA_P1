5b27fe84e544d2e4a4e2126352945162fa0a5381
Marcin <88114702+FBI223@users.noreply.github.com>
2024-10-29 16:16:55 +0100

    first 5 tasks

---
+++ nr 3 _v2/cmake-build-debug/CMakeFiles/nr_3__v2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nr_3__v2.dir/main.cpp.obj"
+  "libnr_3__v2.dll.a"
+  "nr_3__v2.exe"
+  "nr_3__v2.exe.manifest"
+  "nr_3__v2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nr_3__v2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
