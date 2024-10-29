443d173d4f7d0954c59d9939cb7c116c4795cfde
Marcin <88114702+FBI223@users.noreply.github.com>
2024-10-29 16:09:51 +0100

    first 2 tasks

---
+++ zadanie nr 2 ( tabbb )/cmake-build-debug/CMakeFiles/zadanie_nr_2___tabbb__.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zadanie_nr_2___tabbb__.dir/main.cpp.obj"
+  "libzadanie_nr_2___tabbb__.dll.a"
+  "zadanie_nr_2___tabbb__.exe"
+  "zadanie_nr_2___tabbb__.exe.manifest"
+  "zadanie_nr_2___tabbb__.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/zadanie_nr_2___tabbb__.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
